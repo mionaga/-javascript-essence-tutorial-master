f72a4f69820aec532f34ca2d9c606a052e832eeb
ItoKona <lupinmine.1217@gmail.com>
2024-04-19 17:38:32 +0900

    about proto

--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -6,3 +6,14 @@ function Person(name, age) {
 Person.prototype.hello = function() {
   console.log('hello ' + this.name);
 }
+
+function Japanese(name, age){
+  Person.call(this, name, age )
+}
+
+Japanese.prototype = Object.create(Person.prototype);
+
+const taro = new Japanese('Taro', 18);
+console.log(taro);
+
+taro.hello();
